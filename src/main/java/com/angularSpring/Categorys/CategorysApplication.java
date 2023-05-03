9de627eff028677962b57744587df03d33bc0414
leminhosdev <lucaslemos8294@gmail.com>
2023-05-03 13:40:08 -0300

    begining

---
+++ src/main/java/com/angularSpring/Categorys/CategorysApplication.java
@@ -0,0 +1,13 @@
+package com.angularSpring.Categorys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angularSpring/Categorys/CategorysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularSpring.Categorys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
